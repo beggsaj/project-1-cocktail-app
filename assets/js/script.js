0c74ab7f30edd998e8cdedaafe58d622b7f8cf02
Abi Beggs <beggsaj@gmail.com>
2021-11-18 20:19:13 -0600

    updated to display recipe and drink names on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,47 @@
 var drinkButton = document.querySelector('.drink');
 var foodButton = document.querySelector('.food');
+var drinksContainer = document.getElementById('drinks');
+var mealContainer = document.getElementById('meals');
 
-drinkButton.addEventListener('click',function randomDrink() {
+drinkButton.addEventListener('click', function randomDrink() {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
         .then(
             function (response) {
-                if (response.status !== 200) {
-                    console.log('success');
-                    return;
-                }
-
-                response.json().then(function (data) {
-                    console.log(data);
-                });
+                return response.json()
             }
         )
+        .then(function (data) {
+            console.log(data.drinks);
+            let drink = data.drinks[0]
+                var drinkChoice = document.createElement('h3');
+                drinkChoice.textContent = drink.strDrink;
+                drinksContainer.append(drinkChoice);
+        })
         .catch(function (err) {
             console.log('fetch error', err);
         });
+
+    
+
+
 })
 
 
-foodButton.addEventListener('click',function randomMeal() {
+foodButton.addEventListener('click', function randomMeal() {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('success');
-                    return;
-                }
-
-                response.json().then(function (data) {
-                    console.log(data);
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('fetch error', err);
-        });
+    .then(
+        function (response) {
+            return response.json()
+        }
+    )
+    .then(function (data) {
+        console.log(data.meals);
+        let meal = data.meals[0]
+            var mealChoice = document.createElement('h3');
+            mealChoice.textContent = meal.strMeal;
+            mealContainer.append(mealChoice);
+    })
+    .catch(function (err) {
+        console.log('fetch error', err);
+    });
 })
