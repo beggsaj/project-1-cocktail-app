1eb1c2f05dd5dbccdd224495ebde39761b702a5a
Abi Beggs <beggsaj@gmail.com>
2021-11-18 18:29:04 -0600

    added buttons to generate random drinks/meals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,41 @@
-function randomDrink(){
+var drinkButton = document.querySelector('.drink');
+var foodButton = document.querySelector('.food');
+
+drinkButton.addEventListener('click',function randomDrink() {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    .then(
-        function(response) {
-            if (response.status !== 200) {
-                console.log('success');
-                return;
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('success');
+                    return;
+                }
+
+                response.json().then(function (data) {
+                    console.log(data);
+                });
             }
-
-            response.json().then(function(data) {
-                console.log(data);
-            });
-        }
-    )
-    .catch(function(err){
-        console.log('fetch error', err);
-    });
-}
+        )
+        .catch(function (err) {
+            console.log('fetch error', err);
+        });
+})
 
 
-
-function randomMeal(){
+foodButton.addEventListener('click',function randomMeal() {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(
-        function(response) {
-            if (response.status !== 200) {
-                console.log('success');
-                return;
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('success');
+                    return;
+                }
+
+                response.json().then(function (data) {
+                    console.log(data);
+                });
             }
-
-            response.json().then(function(data) {
-                console.log(data);
-            });
-        }
-    )
-    .catch(function(err){
-        console.log('fetch error', err);
-    });
-}
-
-randomDrink();
-randomMeal();
+        )
+        .catch(function (err) {
+            console.log('fetch error', err);
+        });
+})
