41cd3044150fed001722178866c237ab5466e13f
Abi Beggs <beggsaj@gmail.com>
2021-11-24 19:31:12 -0600

    dropdown added

--- assets/js/food.js
+++ assets/js/food.js
@@ -1,7 +1,7 @@
 var mealButton = document.getElementById('meal');
 var mealsContainer = document.getElementById('meals');
 var searchButton = document.getElementById('searchButton');
-var input = document.getElementById('input_text');
+var input = document.getElementById('input');
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
